1eb243f99a85e644b3cb736d8bb27abd45a865f0
TANK\Reda <gicque@netino.com>
2022-04-11 11:41:37 +0200

    change tunr server

--- static/assets/call.js
+++ static/assets/call.js
@@ -126,8 +126,8 @@ function hangup_call(){
 let pcConfig = {
     "iceServers":
         [
-            { "url": "stun.l.google.com:19302" },
-            { "url": "stun1.l.google.com:19302" },
+            { "url": "stun:stun.l.google.com:19302" },
+            { "url": "stun:stun1.l.google.com:19302" },
             //{ "url": "stun:stun.jap.bloggernepal.com:5349" },
             // {
             //     "url": "turn:turn.jap.bloggernepal.com:5349",
